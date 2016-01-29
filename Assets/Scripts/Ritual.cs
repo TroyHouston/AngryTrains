03bae5e61526f2d1bbd6a77491302ffd7115b8e2
Troy Houston <troy.houston@outlook.com>
2016-01-29 23:28:13 +1300

    WIP Ritual Generator

---
+++ Assets/Scripts/Ritual.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Container object for a Ritual group.
+public class Ritual : MonoBehaviour {
+    
+    public GameObject[] ritualGoers {get; set;}
+    public int ritualSize {get; set;}
+    public Vector2 ritualCentre {get; set;}
+
+}
---
+++ Assets/Scripts/RitualGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualGenerator : MonoBehaviour {
+
+    public int ritualCount = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    // TODO Create function for generating set number of rituals
+    void GenerateRituals() {
+        for (int i = 0; i < ritualCount; i++) {
+            GenerateRitualValues();
+            CreateRitualCircle();
+        }
+    }
+    
+    // TODO FUNCTION Create a circle of RitualGoers based on a central position & ritual size:    
+    void CreateRitualCircle() {
+        // TODO Move this method to the Ritual class.
+        // return array of RitualGoer objects to store into the Ritual object
+    } 
+    
+    // TODO FUNCTION Randomize ritual central position & size: 
+    // Centre not near edge.
+    // Centre not same place as another centre.
+    void GenerateRitualValues() {
+        // TODO actually move this to the constructor of the Ritual object.
+    }     
+     
+}
