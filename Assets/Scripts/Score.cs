155bdc7b784b625b5bd97eb37bf8c88cdd5a9b72
josh2g <JoshuaNHood@gmail.com>
2016-01-30 14:23:57 +1300

    Added UI text scripts, improved fonts, added main menu buttons.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	private Text scoreText;
+	private string scoreString; 
+	private int scoreValue;
+
+	// Use this for initialization
+	private void Start () {
+		scoreString = "Score: ";
+		scoreValue = 0;
+		scoreText = GetComponent<Text> ();
+	}
+
+	public void villagerHit () {
+		scoreValue += 15;
+		scoreText.text = scoreString + scoreValue;
+	}
+
+	public void villageWipeout () {
+		scoreValue += 80;
+		scoreText.text = scoreString + scoreValue;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+}
